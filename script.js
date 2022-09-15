2f893c09b11f41f380cec12be0232ab92ff62b23
julias42 <artyom-zhulikov@yandex.ru>
2022-09-15 15:35:38 +0700

    js 1.1

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
 "use strict"
+  
+const products =[
+  {title: "mouse", price:150},
+  {title: "mat", price:50},
+  {title: "book", price:1050},
+  {title: "mouses", price:250},
+];
+
+const renderProductsItem = (title,price)=>{
+  return `<div class="products__product">
+  <img src="image/product_mouse.jpg" alt="photo">
+  <h1 class="products__product_name">${title}</h1>
+  <p>${price}</p>
+</div>`
+};
+
+const renderProductsList = (list)=>{
+  let productList = list.map(item=>renderProductsItem(item.title, item.price))
+  .join("");
+  document.querySelector('.header__basket')
+  .addEventListener('click',(event)=>{
+    event.target.innerHTML = productList;
+  });
+}
+renderProductsList(products);
