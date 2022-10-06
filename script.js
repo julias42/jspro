84b906804189b2a19a6f918d1483762ea6572e08
julias42 <artyom-zhulikov@yandex.ru>
2022-10-06 21:22:21 +0700

    js 1.4

--- script.js
+++ script.js
@@ -1,31 +1,45 @@
 "use strict"
 
-const butEl = document.querySelector('.header__basket');
-const productsEl = document.querySelector('.products');
-  
-const products =[
-  {title: "mouse", price:150},
-  {title: "mat", price:50},
-  {title: "book", price:1050},
-  {title: "mouses", price:250},
-];
+class GoodsList {
+  constructor() {
+    this.goods = [];
+  }
+  fetchgoods() {
+    this.goods = [
+      { title: "Mouse", price: 150 },
+      { title: "Mat", price: 50 },
+      { title: "Book", price: 1050 },
+      { title: "Gamepads", price: 250 },
+    ];
+  }
+  render() {
+    let listHtml = '';
+    this.goods.forEach(good => {
+      const goodItem = new GoodsItem(good.title, good.price);
+      listHtml += goodItem.render();
+    });
+    document.querySelector('.products').innerHTML = listHtml;
+  }
+}
 
-const renderProductsItem = ()=>{
-  return `
+class GoodsItem {
+  constructor(title, price) {
+    this.title = title;
+    this.price = price;
+  }
+  render() {
+    return `
   <div class="products__product">
     <img src="image/product_mouse.jpg" alt="photo">
     <h1 class="products__product_name">${this.title}</h1>
-    <p class="products__product_price">${this.price} у.е</p>
+    <h4 class="products__product_price">${this.price} у.е</h4>
     <button class="products__product_but">Добавить</button>
   </div>`
-};
-
-const renderProductsList = (list)=>{
-  let productList = list.map(item=>renderProductsItem(item.title, item.price))
-  .join("");
-  productsEl.innerHTML = productList;
+  };
 }
- renderProductsList(products);
 
+const list = new GoodsList();
+list.fetchgoods();
+list.render();
 
 
